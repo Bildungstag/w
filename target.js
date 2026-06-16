76f7d2550182b9d5bcc26ec95ca45402ac2dd9fb
Bildungstag <straub_julian@t-online.de>
2026-06-16 21:27:24 +0200

    Initiale target.js (wird automatisch aktualisiert)

---
+++ target.js
@@ -0,0 +1,5 @@
+/* Wird automatisch vom tunnel_manager.py aktualisiert.
+ * Enthält die aktuelle Cloudflare-Tunnel-URL, zu der redirect.html weiterleitet.
+ * NICHT MANUELL BEARBEITEN – wird bei jedem Tunnel-Neustart überschrieben.
+ */
+window.__TARGET = "https://platzhalter.trycloudflare.com";
